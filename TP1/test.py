f698181ba2f492de79af4462107e9ba77ce1fc75
Laurent1310 <laurent.juhasz@univ-rouen.fr>
2023-09-27 15:39:34 +0200

    modification test py

--- TP1/test.py
+++ TP1/test.py
@@ -6,5 +6,5 @@ def test_1 ( ) :
 	assert f . puissance ( 2 , 2 ) == 4
 
 def test_2 ( ) :
-        assert f . puissance ( -2 , -3 ) == -0.125
-        assert f . puissance ( -2 , -2 ) == -0.25
+        assert f . puissance ( -2 , 2 ) == -4
+        assert f . puissance ( -2 , 3 ) == -8
